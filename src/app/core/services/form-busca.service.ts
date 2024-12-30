82b25b5754ae7c78e88c5c4641f4a91b8ce4c5dd
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2024-12-30 14:02:17 -0300

    iniciei-o-formBusca

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,15 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormBuscaService {
   formBusca: FormGroup
-  constructor(private httpClient: HttpClient) {
+  constructor() {
     this.formBusca = new FormGroup({
-      
+      somenteIda: new FormControl(false)
+      //o new antes normalmente é pra quando é uma classe. Por exemplo, o meu FormControl é uma classe, pra inicializa-lo eu começo com new
     })
    }
 }
