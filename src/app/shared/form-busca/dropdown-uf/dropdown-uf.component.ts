69ecd4a845bc49632d45d1e202c5878c671debcb
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2025-01-01 20:32:05 -0300

    autocomplete-onChange

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, startWith, map } from 'rxjs';
 import { UnidadeFederativaService } from 'src/app/core/services/unidade-federativa.service';
 import { UnidadeFederativa } from 'src/app/core/types/type';
 
@@ -7,23 +9,33 @@ import { UnidadeFederativa } from 'src/app/core/types/type';
   templateUrl: './dropdown-uf.component.html',
   styleUrls: ['./dropdown-uf.component.scss']
 })
-export class DropdownUfComponent implements OnInit{
- 
-  @Input() label!: string //o ponto de exclamação é pra tirar o problema de nao inicialização da variável label
-  @Input() iconePrefixo!: string
+export class DropdownUfComponent implements OnInit {
+  myControl = new FormControl('');
+  @Input() label!: string; // o ponto de exclamação é para tirar o problema de não inicialização da variável label
+  @Input() iconePrefixo!: string;
+
   unidadesFederativas: UnidadeFederativa[] = [];
-  filteredOptions = [];
-  constructor(
-    private unidadeFederativaService: UnidadeFederativaService
-  ){
-    
-  }
+  filteredOptions!: Observable<UnidadeFederativa[]>;
+
+  constructor(private unidadeFederativaService: UnidadeFederativaService) {}
+
   ngOnInit(): void {
-    this.unidadeFederativaService.listar()
-        .subscribe(dados => {
-            this.unidadesFederativas = dados;
-        })
+    // Busca inicial da lista completa de unidades federativas
+    this.unidadeFederativaService.listar().subscribe((dados) => {
+      this.unidadesFederativas = dados;
+
+      // Configura o Observable para filtrar os resultados
+      this.filteredOptions = this.myControl.valueChanges.pipe(
+        startWith(''),
+        map((value) => this._filter(value || ''))
+      );
+    });
+  }
+
+  private _filter(value: string): UnidadeFederativa[] {
+    const filterValue = value.toLowerCase();
+    return this.unidadesFederativas.filter((option) =>
+      option.nome.toLowerCase().includes(filterValue)
+    );
+  }
 }
-  
-}
-    
