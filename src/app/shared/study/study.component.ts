3a30d506e976ae13b0b8c275060abfc5938d9e04
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2025-01-06 13:11:09 -0300

    ..

--- src/app/shared/study/study.component.ts
+++ src/app/shared/study/study.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-study',
-  templateUrl: './study.component.html',
-  styleUrls: ['./study.component.scss']
+@Injectable({
+  providedIn: 'root'
 })
-export class StudyComponent {
+export class UserService implements OnInit{
+  inputProdutor = '';
+  constructor(){ }
 
+  private timer = new Promise<string>((resolve, reject) => {
+    // resolve e reject são funções e não parametros
+    setTimeout(() => {
+      // depois de 5 segundos eu chamo o medodo resolver que me retorna a string "Resolvido" visto que eu falei que o tipo seria string. Se eu quisesse que ele mandasse um number ou um tipo especifico seria new Promise<number/tipoespecifico> e o resolve(number/tipoespecifico)
+      resolve("Resolvido")
+  }, 5000)
+  })
+
+  ngOnInit(): void {
+    // quando o componente é inicializado a primeira vez tudo que ta aqui é carregado 
+    this.timer.then(value /*nesse caso o nosso value é o que ta dentro de resolve */ => {
+      // O then é pra puxar o que se espera da promise que nesse caso é o nosso resolver. é o que deu certo
+      console.log(value); //vai mostrar o "Resolvido"
+    })
+
+  }
 }
