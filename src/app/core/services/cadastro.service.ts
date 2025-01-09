a2370451d560ecdef7f5c81d7dde5edf84039fbb
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2025-01-09 10:58:43 -0300

    feat: cadastro-service-check

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -15,4 +15,20 @@ export class CadastroService {
   cadastrar(pessoaUsuaria: PessoaUsuaria): Observable<PessoaUsuaria> {
     return this.http.post<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro`, pessoaUsuaria);
   }
+  buscarCadastro(token: string): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.get<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, {headers});
+    //método pra buscar os dados do usuário pra página de perfil. Esse método buscar o usuário baseado no seu token. A rota exige que seja passado o headers como authorização 
+  }
+
+  editarCadastro(pessoaUsuaria: PessoaUsuaria,token: string): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.patch<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, pessoaUsuaria, {headers});
+    //método de mudança de dados do usuário passando o novo body com os dados do tipo PessoaUsuaria e seus campos com suas respectivas mudanças. Passar o headers como authorization tb 
+  }
+  
 }
